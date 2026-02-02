d678ebf0e19c4df838a2ae54319e2362f031d48c
Maksim <122995797+GALANTfin@users.noreply.github.com>
2026-02-02 20:38:03 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const buttons = document.querySelectorAll(".open-modal-btn");
+
+  buttons.forEach(btn => {
+    const modalId = btn.getAttribute("data-modal");
+    const modal = document.getElementById(modalId);
+    const close = modal.querySelector(".close");
+
+    btn.addEventListener("click", () => {
+      modal.style.display = "block";
+      document.body.style.overflow = "hidden";
+    });
+
+    close.addEventListener("click", () => {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto";
+    });
+
+    window.addEventListener("click", (e) => {
+      if (e.target === modal) {
+        modal.style.display = "none";
+        document.body.style.overflow = "auto";
+      }
+    });
+  });
+});
