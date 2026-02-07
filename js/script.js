5ab0175972c79aa34f232e705bc505bcf9c4c69e
Maksim <122995797+GALANTfin@users.noreply.github.com>
2026-02-07 10:57:42 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,18 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
     const close = modal.querySelector(".close");
 
     btn.addEventListener("click", () => {
+      modal.removeAttribute("hidden");
       modal.style.display = "block";
       document.body.style.overflow = "hidden";
     });
 
     close.addEventListener("click", () => {
       modal.style.display = "none";
+      modal.setAttribute("hidden", "");
       document.body.style.overflow = "auto";
     });
 
     window.addEventListener("click", (e) => {
       if (e.target === modal) {
         modal.style.display = "none";
+        modal.setAttribute("hidden", "");
         document.body.style.overflow = "auto";
       }
     });
